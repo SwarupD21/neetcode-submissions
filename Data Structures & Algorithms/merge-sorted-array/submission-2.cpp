7390a44a2a457be274dd130f6243201dc12a3cea
Swarup Dash <161138214+SwarupD21@users.noreply.github.com>
2026-06-07 23:58:50 +0530

    Add: merge-sorted-array - submission-2

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = 0;
+        int j = 0;
+        while(i<m && j<n){
+            if(nums1[i]>nums2[j]){
+                swap(nums1[i],nums2[j]);
+                i++;
+            }
+            else{
+                i++;
+            }
+        }
+        while(j<n){
+            swap(nums1[i],nums2[j]);
+            i++;
+            j++;
+        }
+        sort(nums1.begin(),nums1.end());
+    }
+};
